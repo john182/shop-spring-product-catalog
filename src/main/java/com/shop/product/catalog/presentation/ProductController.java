1d4b493c225f7935d3a69f0ea91ba12de05f0593
chronosinfo <chronosinfo@chronosinfo.com.br>
2026-05-16 18:08:35 -0300

    Contrato e recurso de listagem

--- src/main/java/com/shop/product/catalog/presentation/ProductController.java
+++ src/main/java/com/shop/product/catalog/presentation/ProductController.java
@@ -1,12 +1,14 @@
 package com.shop.product.catalog.presentation;
 
 import jakarta.annotation.security.PermitAll;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -15,8 +17,7 @@ public class ProductController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @PreAuthorize("permitAll()")
-    public ProductDetailOutput create(@RequestBody ProductInput input) {
+    public ProductDetailOutput create(@RequestBody @Valid ProductInput input) {
         return ProductDetailOutput.builder()
                 .id(UUID.randomUUID())
                 .addedAt(OffsetDateTime.now())
@@ -54,6 +55,53 @@ public class ProductController {
                         .id(UUID.randomUUID())
                         .name("Notebook")
                         .build())
+                .build();
+    }
+
+    @GetMapping
+    public PageModel<ProductDetailOutput> filter(
+            @RequestParam(name = "size", required = false) Integer size,
+            @RequestParam(name = "number", required = false) Integer number
+    ) {
+        return PageModel.<ProductDetailOutput>builder()
+                .number(0)
+                .size(size)
+                .totalPages(1)
+                .totalElements(2)
+                .content(
+                        List.of(
+                                ProductDetailOutput.builder()
+                                        .id(UUID.randomUUID())
+                                        .addedAt(OffsetDateTime.now())
+                                        .name("Notebook X11")
+                                        .brand("Deep Diver")
+                                        .description("A Gamer Notebook")
+                                        .regularPrice(new BigDecimal("1500.00"))
+                                        .salePrice(new BigDecimal("1000.00"))
+                                        .inStock(true)
+                                        .enabled(true)
+                                        .category(CategoryMinimalOutput.builder()
+                                                .id(UUID.randomUUID())
+                                                .name("Notebook")
+                                                .build())
+                                        .build(),
+                                ProductDetailOutput.builder()
+                                        .id(UUID.randomUUID())
+                                        .addedAt(OffsetDateTime.now())
+                                        .name("Desktop I9000")
+                                        .brand("Deep Diver")
+                                        .description("A Gamer Desktop")
+                                        .regularPrice(new BigDecimal("3500.00"))
+                                        .salePrice(new BigDecimal("3000.00"))
+                                        .inStock(false)
+                                        .enabled(true)
+                                        .category(CategoryMinimalOutput.builder()
+                                                .id(UUID.randomUUID())
+                                                .name("Desktop")
+                                                .build())
+                                        .build()
+                        )
+                )
                 .build();
     }
 
